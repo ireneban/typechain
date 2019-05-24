ad0a6be5895324523858198e9ea88d5be71efb77
Mijeong Ban <mban1@stevens.edu>
2019-05-25 07:31:29 +0900

    create a block

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ class Block {
     timestamp: number,
     data: string
   ): string =>
-    CryptoJS.SHA25(index + previousHash + timestamp + data).toString();
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   constructor(
     index: number,
     hash: string,
@@ -40,5 +40,28 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+
+  return newBlock;
+};
+
+console.log(createNewBlock("Hello"), createNewBlock("Bye"));
 
 export {};
