7cca05c00b531f000ef76b85bb6899401525e153
Mijeong Ban <mban1@stevens.edu>
2019-05-25 06:35:59 +0900

    basic setup

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "Mijeong",
+  age = 27,
+  gender = "female";
+
+const sayHi = (name, age, gender) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
